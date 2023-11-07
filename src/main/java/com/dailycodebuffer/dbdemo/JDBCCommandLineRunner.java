89e4bb2ba6cb4415b3b4c1bcbee30c46fc7eccf9
Vishrut Majmudar <v.h.majmudar@gmail.com>
2023-11-07 08:06:04 +0530

    Inserting Hardcoded Data using Spring JDBC

--- src/main/java/com/dailycodebuffer/dbdemo/JDBCCommandLineRunner.java
+++ src/main/java/com/dailycodebuffer/dbdemo/JDBCCommandLineRunner.java
@@ -2,8 +2,13 @@ package com.dailycodebuffer.dbdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 public class JDBCCommandLineRunner implements CommandLineRunner {
@@ -11,7 +16,15 @@ public class JDBCCommandLineRunner implements CommandLineRunner {
     JdbcTemplate jdbcTemplate;
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("In JDBCCommandLineRunner");
+        //Lets truncate table first for easy reference and multiple runs.
+        jdbcTemplate.execute("truncate table ORDER_DETAILS");
+        insertHardCodeData();
 
+    }
+    public void insertHardCodeData() {
+        System.out.println("In JDBCCommandLineRunner For inserting hardcoded data.");
+        jdbcTemplate.execute("INSERT INTO ORDER_DETAILS VALUES(1,'Mobile','customer','bengaluru')");
+        System.out.println("Inserted hardcode data.");
+        // go to mysql and check record
     }
 }
